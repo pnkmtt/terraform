c679ec652629f6f10bb19ea22b05a9a2d6bf0c19
Matt Panik <pnkmtt@gmail.com>
2017-07-08 10:33:24 -0400

    fixed final snapshot

--- 03/stage/data-stores/mysql/main.tf
+++ 03/stage/data-stores/mysql/main.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "example" {
 	name	=	"example_database"
 	username	=	"admin"
 	password	=	"${var.db_password}"
+	skip_final_snapshot	=	true
 }
 data "terraform_remote_state" "db" {
 	backend = "s3"
